cf5aedeac2e49941e948620cfb0b6599732369b5
tnRaro <deokji53@gmail.com>
2021-05-09 17:58:52 +0900

    .

--- tools/test-script.js
+++ tools/test-script.js
@@ -3,42 +3,34 @@
   const state = {};
   const scenario = [
     {
-      url: "/api/room/title/시나리오 테스트",
+      url: "/api/room/title/새로운 방 제목",
       method: "POST",
       cb: async (res) => (state.roomId = res.roomId),
     },
     { url: () => `/api/rooms/${state.roomId}` },
     {
-      url: () => `/api/rooms/${state.roomId}/participants/탱템버린`,
+      url: () => `/api/rooms/${state.roomId}/participants/참가자이름`,
       method: "POST",
       cb: async (res) => (state.hostId = res.participantId),
     },
-    {
-      url: () => `/api/rooms/${state.roomId}`,
-      headers: () => ({ authorization: "X-API-KEY " + state.hostId }),
-    },
-    {
-      url: () => `/api/rooms/${state.roomId}`,
-      method: "PUT",
-      headers: () => ({ authorization: "X-API-KEY " + state.hostId }),
-      cb: async (res) => (state.roomId = res.roomId),
-    },
+    { url: () => `/api/rooms/${state.roomId}` },
+    //     { url: () => `/api/rooms/${state.roomId}`, method: "PUT", headers: () => ({ authorization: "X-API-KEY " + state.hostId }), cb: async res => state.roomId = res.roomId },
   ];
   try {
     for (const scena of scenario) {
-      const res = await fetch(
-        typeof scena.url === "function" ? scena.url() : scena.url,
-        {
-          method: scena.method ?? "GET",
-          headers:
-            typeof scena.headers === "function"
-              ? scena.headers()
-              : scena.headers ?? {},
-        },
-      );
-      console.info(res.status);
+      const url = typeof scena.url === "function" ? scena.url() : scena.url;
+      const method = scena.method ?? "GET";
+      const headers =
+        typeof scena.headers === "function"
+          ? scena.headers()
+          : scena.headers ?? {};
+      const res = await fetch(url, { method, headers });
+      console.info(method, url);
+      if (res.status >= 400) {
+        throw res;
+      }
       const data = await res.json();
-      console.log(data);
+      console.log(res.status, data);
       history.push(data);
       if (typeof scena.cb === "function") await scena.cb(data);
     }

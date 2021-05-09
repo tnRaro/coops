17a7d0203286ba399d0d3c8507b661c57e6f4b8d
tnRaro <deokji53@gmail.com>
2021-05-09 16:43:22 +0900

    Implement POST /api/rooms/[roomId]/participants/[nickname]

--- services/web/src/pages/api/rooms/[roomId]/participants/[nickname]/index.ts
+++ services/web/src/pages/api/rooms/[roomId]/participants/[nickname]/index.ts
@@ -30,11 +30,46 @@
  */
 
 import { HttpError } from "../../../../../../app/errors/HttpError";
+import { ModelError } from "../../../../../../app/errors/ModelError";
 import { apiRouter } from "../../../../../../app/utils/apiRouter";
+import { HttpResult } from "../../../../../../app/utils/HttpResult";
+import { enterParticipantAtRoom } from "../../../../../../redis/models/participants";
+import { getRedisClient } from "../../../../../../redis/utils/getRedisClient";
 
+interface Query {
+  roomId: string;
+  nickname: string;
+}
+const isQuery = (query: unknown): query is Query => {
+  if (query == null) {
+    return false;
+  }
+  return (
+    typeof (query as Query).roomId === "string" &&
+    typeof (query as Query).nickname === "string"
+  );
+};
 export default apiRouter({
   POST: async (req, res) => {
-    throw new HttpError(501);
+    if (!isQuery(req.query)) {
+      throw new HttpError(400);
+    }
+    const [redis, quit] = getRedisClient();
+    try {
+      const participant = await enterParticipantAtRoom(
+        redis,
+        req.query.roomId,
+        req.query.nickname,
+      );
+      await quit();
+      return new HttpResult(participant, 201);
+    } catch (error) {
+      await quit();
+      if (error instanceof ModelError) {
+        throw new HttpError(error.code, error.message);
+      }
+      throw error;
+    }
   },
   DELETE: async (req, res) => {
     throw new HttpError(501);

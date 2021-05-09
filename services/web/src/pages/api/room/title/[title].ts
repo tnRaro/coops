5bbda28ef60ae5171f5cbf2257630cb056351463
tnRaro <deokji53@gmail.com>
2021-05-09 12:10:30 +0900

    Implement POST /api/room/title/[title]

--- services/web/src/pages/api/room/title/[title].ts
+++ services/web/src/pages/api/room/title/[title].ts
@@ -15,10 +15,38 @@
  */
 
 import { HttpError } from "../../../../app/errors/HttpError";
+import { ModelError } from "../../../../app/errors/ModelError";
 import { apiRouter } from "../../../../app/utils/apiRouter";
+import { HttpResult } from "../../../../app/utils/HttpResult";
+import { createRoom } from "../../../../redis/models/rooms";
+import { getRedisClient } from "../../../../redis/utils/getRedisClient";
+
+interface Query {
+  title: string;
+}
+const isQuery = (query: unknown): query is Query => {
+  if (query == null) {
+    return false;
+  }
+  return typeof (query as Query).title === "string";
+};
 
 export default apiRouter({
   POST: async (req, res) => {
-    throw new HttpError(501);
+    if (!isQuery(req.query)) {
+      throw new HttpError(400);
+    }
+    const [redis, quit] = getRedisClient();
+    try {
+      const roomId = await createRoom(redis, req.query.title);
+      await quit();
+      return new HttpResult({ roomId }, 201);
+    } catch (error) {
+      await quit();
+      if (error instanceof ModelError) {
+        throw new HttpError(error.code, error.message);
+      }
+      throw error;
+    }
   },
 });

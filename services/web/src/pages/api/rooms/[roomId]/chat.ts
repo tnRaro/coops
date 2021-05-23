2f6bef7ad9b57abace00c9fd1b81095e4e24b84d
tnRaro <deokji53@gmail.com>
2021-05-23 10:11:21 +0900

    Implement chat API at web

--- services/web/src/pages/api/rooms/[roomId]/chat.ts
+++ services/web/src/pages/api/rooms/[roomId]/chat.ts
@@ -18,15 +18,44 @@
  * ```
  */
 
+import { HttpResult } from "@coops/core";
 import { HttpError } from "@coops/error";
+import * as logic from "@coops/logic";
+import * as redis from "@coops/redis";
 import { withRedisClient } from "@coops/redis";
 
 import { apiRouter } from "../../../../app/utils/apiRouter";
+import { auth } from "../../../../app/utils/auth";
+import { isRoomIdQuery } from "../../../../app/utils/queries";
 
 export default apiRouter({
   POST: async (req, res) => {
+    if (!isRoomIdQuery(req.query)) {
+      throw new HttpError(400);
+    }
+    const { roomId } = req.query;
+    const { message } = JSON.parse(req.body) as { message: string };
+    const authorId = auth(req, `Access to the room: ${roomId}`);
     return withRedisClient(async (client) => {
-      throw new HttpError(501);
+      if (!(await logic.participant.isParticipant(client, roomId, authorId))) {
+        throw new HttpError(403);
+      }
+      await logic.chat.appendChat(client, roomId, message, authorId);
+      return new HttpResult(null, 201);
+    });
+  },
+  GET: async (req, res) => {
+    if (!isRoomIdQuery(req.query)) {
+      throw new HttpError(400);
+    }
+    const { roomId } = req.query;
+    const authorId = auth(req, `Access to the room: ${roomId}`);
+    return withRedisClient(async (client) => {
+      if (!(await logic.participant.isParticipant(client, roomId, authorId))) {
+        throw new HttpError(403);
+      }
+      const chats = await logic.chat.findAllChats(client, roomId);
+      return chats;
     });
   },
 });

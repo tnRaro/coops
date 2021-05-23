3fe0c39a212428dc2d47816cb556909175185243
tnRaro <deokji53@gmail.com>
2021-05-23 10:09:58 +0900

    Implement chatting at redis

--- services/redis/src/index.ts
+++ services/redis/src/index.ts
@@ -2,3 +2,5 @@ export { getRedisClient } from "./getRedisClient";
 export { withRedisClient } from "./withRedisClient";
 export * as room from "./room";
 export * as participant from "./participant";
+export * as pubsub from "./pubsub";
+export * as chat from "./chat";

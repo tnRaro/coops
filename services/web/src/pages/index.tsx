5fa5ad9a65b9e53a4b866148b8c80f0758549e0b
tnRaro <deokji53@gmail.com>
2021-06-09 12:43:06 +0900

    Make field receive an invite link

--- services/web/src/pages/index.tsx
+++ services/web/src/pages/index.tsx
@@ -1,6 +1,11 @@
 import { consts } from "@coops/core";
 import { useRouter } from "next/router";
-import React, { useEffect, useState, VoidFunctionComponent } from "react";
+import React, {
+  useEffect,
+  useMemo,
+  useState,
+  VoidFunctionComponent,
+} from "react";
 
 import { Card } from "../app/components/core/Card";
 import { Container } from "../app/components/core/Container";
@@ -16,29 +21,70 @@ import { useResetRoom } from "../app/hooks/useResetRoom";
 type PageProps = unknown;
 const Page: VoidFunctionComponent<PageProps> = (props) => {
   const [title, setTitle] = useState("");
-  const [roomId, setRoomId] = useState("");
+  const [inviteCode, setInviteCode] = useState("");
   const router = useRouter();
   const setOops = useOops();
-  const queryies = useQuery();
+  const queries = useQuery();
   const resetRoom = useResetRoom();
+  const linkReg = useMemo(
+    () => new RegExp(`^${location.origin}/rooms/(.{6})$`),
+    [],
+  );
   useEffect(() => {
     resetRoom();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
+  const enterRoomHandler = () => {
+    let code = "";
+    const matchedLink = inviteCode.trim().match(linkReg);
+    if (matchedLink == null) {
+      code = inviteCode.trim().toUpperCase();
+    } else {
+      code = matchedLink[1];
+    }
+    router.push(`/rooms/${code}`, undefined, { shallow: true });
+  };
+  const createRoomHandler = async () => {
+    if (title.length > consts.room.title.length.max) {
+      setOops(
+        new FrontError(
+          `방 제목은 최대 ${consts.room.title.length.max}자입니다`,
+        ),
+      );
+    } else if (title.length < consts.room.title.length.min) {
+      setOops(
+        new FrontError(
+          `방 제목은 최소 ${consts.room.title.length.min}자 이상입니다`,
+        ),
+      );
+    } else {
+      try {
+        const { roomId } = await queries.createRoom(title);
+        router.push(`/rooms/${roomId}`, undefined, { shallow: true });
+      } catch (error) {
+        // eslint-disable-next-line no-console
+        console.error(error);
+      }
+    }
+  };
   return (
     <Container>
       <Card title="저에게 집결하세요!">
         <Input
           placeholder="초대 코드를 입력해주세요"
-          value={roomId}
-          onChange={(event) => setRoomId(event.target.value)}
+          minLength={6}
+          value={inviteCode}
+          onChange={(event) => setInviteCode(event.target.value)}
+          onKeyPress={(event) => {
+            if (event.key === "Enter") {
+              enterRoomHandler();
+            }
+          }}
         />
         <Button
           color="gray"
-          onClick={() => {
-            router.push(`/rooms/${roomId}`, undefined, { shallow: true });
-          }}
-          disabled={roomId.length < 6}
+          onClick={enterRoomHandler}
+          disabled={inviteCode.length < 6}
         >
           방 참가하기
         </Button>
@@ -57,26 +103,13 @@ const Page: VoidFunctionComponent<PageProps> = (props) => {
           placeholder="방 제목을 입력해주세요"
           value={title}
           onChange={(event) => setTitle(event.target.value)}
-        />
-        <Button
-          onClick={async () => {
-            if (title.length > consts.room.title.length.max) {
-              setOops(new FrontError("방 제목은 최대 10자 입니다"));
-            } else if (title.length < consts.room.title.length.min) {
-              setOops(new FrontError("방 제목은 최소 2자 이상입니다"));
-            } else {
-              try {
-                const { roomId } = await queryies.createRoom(title);
-                router.push(`/rooms/${roomId}`, undefined, { shallow: true });
-              } catch (error) {
-                // eslint-disable-next-line no-console
-                console.error(error);
-              }
+          onKeyPress={(event) => {
+            if (event.key === "Enter") {
+              createRoomHandler();
             }
           }}
-        >
-          방 만들기
-        </Button>
+        />
+        <Button onClick={createRoomHandler}>방 만들기</Button>
       </Card>
     </Container>
   );

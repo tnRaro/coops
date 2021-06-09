8e4a915bfd8e561ea765c822158b29d168eff3db
tnRaro <deokji53@gmail.com>
2021-06-10 01:50:41 +0900

    Make editable fields at room detail

--- services/web/src/app/components/coops/cards/RoomDetailCard.tsx
+++ services/web/src/app/components/coops/cards/RoomDetailCard.tsx
@@ -1,11 +1,9 @@
-/* eslint-disable @shopify/jsx-no-complex-expressions */
-import { useAtom } from "jotai";
 import { useAtomValue } from "jotai/utils";
 import React, { useEffect, useRef, useState } from "react";
 
 import {
   authorIdAtom,
-  participantsAtom,
+  isHostAtom,
   roomDescriptionAtom,
   roomIdAtom,
   roomTitleAtom,
@@ -20,27 +18,38 @@ export const RoomDetailCard: React.VFC<RoomDetailCardProps> = () => {
   const queries = useQuery();
   const roomId = useAtomValue(roomIdAtom);
   const authorId = useAtomValue(authorIdAtom);
-  const participants = useAtomValue(participantsAtom);
-  const [title, setTitle] = useAtom(roomTitleAtom);
-  const [description, setDescription] = useAtom(roomDescriptionAtom);
+  const isHost = useAtomValue(isHostAtom);
+  const title = useAtomValue(roomTitleAtom);
+  const description = useAtomValue(roomDescriptionAtom);
+  const [editingTitle, setEditingTitle] = useState("");
+  const [editingDescription, setEditingDescription] = useState("");
   const hasDescription = description != null;
   const descriptionColor = hasDescription ? "text66" : "text33";
-  const [isTitleEdit, setIsTitleEdit] = useState(false);
-  const [isDescriptionEdit, setIsDescriptionEdit] = useState(false);
+  const [isTitleEditing, setIsTitleEditing] = useState(false);
+  const [isDescriptionEditing, setIsDescriptionEditing] = useState(false);
   const inputRef = useRef<HTMLInputElement>(null);
   useEffect(() => {
+    if (isTitleEditing) {
+      setEditingTitle(title ?? "");
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [isTitleEditing]);
+  useEffect(() => {
+    if (isDescriptionEditing) {
+      setEditingTitle(description ?? "");
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [isDescriptionEditing]);
+  useEffect(() => {
     inputRef.current?.focus();
-  }, [isDescriptionEdit, isTitleEdit]);
+  }, [isDescriptionEditing, isTitleEditing]);
   if (roomId == null) return null;
   if (authorId == null) return null;
   return (
     <>
-      {isTitleEdit ? (
+      {isTitleEditing ? (
         <Input
           ref={inputRef}
-          onClick={() => {
-            setIsTitleEdit(true);
-          }}
           css={{
             padding: 0,
             fontSize: "$heading4",
@@ -48,65 +57,89 @@ export const RoomDetailCard: React.VFC<RoomDetailCardProps> = () => {
             lineHeight: "$heading",
           }}
           placeholder="방 제목을 입력해주세요"
-          value={title ?? ""}
-          onChange={(event) => setTitle(event.target.value)}
-          onKeyDown={async (event) => {
+          value={editingTitle ?? ""}
+          onChange={(event) => setEditingTitle(event.target.value)}
+          onKeyPress={async (event) => {
             if (event.key === "Enter") {
               try {
                 await queries.updateRoomSettings(roomId, authorId, {
-                  title: title ?? undefined,
+                  title: editingTitle ?? undefined,
                 });
-                setIsTitleEdit(false);
+                setIsTitleEditing(false);
               } catch (error) {
                 // eslint-disable-next-line no-console
                 console.error(error);
               }
             }
           }}
+          onKeyDown={(event) => {
+            if (event.key === "Escape") {
+              setIsTitleEditing(false);
+            }
+          }}
           onBlur={() => {
-            if (title === "") {
-              setIsTitleEdit(false);
+            if (editingTitle === "" || editingTitle === (title ?? "")) {
+              setIsTitleEditing(false);
             }
           }}
         />
       ) : (
-        <Heading4 onClick={() => setIsTitleEdit(true)}>{title}</Heading4>
+        <Heading4
+          onClick={() => {
+            if (isHost) {
+              setIsTitleEditing(true);
+            }
+          }}
+        >
+          {title}
+        </Heading4>
       )}
-      {isDescriptionEdit ? (
+      {isDescriptionEditing ? (
         <Input
           ref={inputRef}
-          onClick={() => {
-            setIsDescriptionEdit(true);
-          }}
           css={{
             padding: 0,
           }}
           placeholder="공지사항을 입력해주세요"
-          value={description ?? ""}
-          onChange={(event) => setDescription(event.target.value)}
-          onKeyDown={async (event) => {
+          value={editingDescription ?? ""}
+          onChange={(event) => setEditingDescription(event.target.value)}
+          onKeyPress={async (event) => {
             if (event.key === "Enter") {
               try {
                 await queries.updateRoomSettings(roomId, authorId, {
-                  description: description ?? undefined,
+                  description: editingDescription ?? undefined,
                 });
-                setIsDescriptionEdit(false);
+                setIsDescriptionEditing(false);
               } catch (error) {
                 // eslint-disable-next-line no-console
                 console.error(error);
               }
+            } else if (event.key === "Escape") {
+              setIsDescriptionEditing(false);
+            }
+          }}
+          onKeyDown={(event) => {
+            if (event.key === "Escape") {
+              setIsTitleEditing(false);
             }
           }}
           onBlur={() => {
-            if (description === "") {
-              setIsDescriptionEdit(false);
+            if (
+              editingDescription === "" ||
+              editingDescription === (description ?? "")
+            ) {
+              setIsDescriptionEditing(false);
             }
           }}
         />
       ) : (
         <Text
           color={descriptionColor}
-          onClick={() => setIsDescriptionEdit(true)}
+          onClick={() => {
+            if (isHost) {
+              setIsDescriptionEditing(true);
+            }
+          }}
         >
           {description ?? "공지사항을 입력해주세요"}
         </Text>

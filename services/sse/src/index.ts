902fc215e176d045d342fe0faefb815caa230adb
tnRaro <deokji53@gmail.com>
2021-06-10 01:24:59 +0900

    Use expressjs and implement disconnect state

--- services/sse/src/index.ts
+++ services/sse/src/index.ts
@@ -1,67 +1,60 @@
-/* eslint-disable require-atomic-updates */
 import util from "util";
-import { PassThrough } from "stream";
 
 import { HttpError, LogicError, UnauthorizedError } from "@coops/error";
 import * as logic from "@coops/logic";
 import * as redis from "@coops/redis";
 import { getRedisClient } from "@coops/redis";
-import cors from "@koa/cors";
-import Koa from "koa";
+import express, { NextFunction } from "express";
 
 import { HttpResult } from "../../core/dist";
 
-const koa = new Koa();
+const app = express();
 
-koa.use(
-  cors({
-    credentials: true,
-  }),
-);
-
-koa.use(async (context, next) => {
-  const pathReg = /^\/sse\/rooms\/([^/]{6})\/stream$/;
-  if (!pathReg.test(context.path)) {
-    return next();
-  }
-  const [client, quit] = getRedisClient();
+app.use("/sse/rooms/:roomId", async (req, res, next) => {
+  const roomId = req.params.roomId;
+  const [pubsub, quit0] = getRedisClient();
+  const [client, quit1] = getRedisClient();
+  const quit = () => {
+    quit0();
+    quit1();
+  };
   try {
-    const { key } = context.query;
-    const [_, roomId] = context.path.match(pathReg)!;
+    const { key } = req.query;
     if (key == null) {
       throw new UnauthorizedError(`Access to the room: ${roomId}`);
     }
     const authorId = key as string;
-    if (!(await logic.participant.isParticipant(client, roomId, authorId))) {
+    if (!(await logic.participant.isParticipant(pubsub, roomId, authorId))) {
       throw new HttpError(403);
     }
     const chatKey = redis.chat.useChatKey(roomId);
     const roomKey = redis.room.keys.useRoomKey(roomId);
     const participantIdKey = redis.participant.keys.useParticipantIdKey(roomId);
-    await redis.pubsub.sub(client, chatKey);
-    await redis.pubsub.sub(client, roomKey);
-    await redis.pubsub.sub(client, participantIdKey);
-    context.request.socket.setTimeout(0);
-    context.req.socket.setNoDelay(true);
-    context.req.socket.setKeepAlive(true);
-    context.set("Content-Type", "text/event-stream");
-    context.set("Cache-Control", "no-cache");
-    context.set("X-Accel-Buffering", "no");
-    context.set("Connection", "keep-alive");
-    const stream = new PassThrough();
-    context.status = 200;
-    context.body = stream;
+    await redis.pubsub.sub(pubsub, chatKey);
+    await redis.pubsub.sub(pubsub, roomKey);
+    await redis.pubsub.sub(pubsub, participantIdKey);
+    req.socket.setTimeout(0);
+    req.socket.setNoDelay(true);
+    req.socket.setKeepAlive(true);
+    res.set({
+      "Content-Type": "text/event-stream",
+      "Cache-Control": "no-cache",
+      "X-Accel-Buffering": "no",
+      Connection: "keep-alive",
+    });
+    res.status(200);
+    res.flushHeaders();
     let isRunning = true;
     const say = (message: string, event?: string) => {
       if (!isRunning) {
         return;
       }
       if (event != null) {
-        stream.write(`event: ${event}\n`);
+        res.write(`event: ${event}\n`);
       }
-      stream.write(`data: ${message}\n\n`);
+      res.write(`data: ${message}\n\n`);
     };
-    client.on("message", (channel, message) => {
+    pubsub.on("message", (channel, message) => {
       switch (channel) {
         case chatKey: {
           say(message, "chat");
@@ -81,40 +74,68 @@ koa.use(async (context, next) => {
         }
       }
     });
-    stream.on("close", async () => {
+    const [nickname] = await redis.participant.CRUD.findParticipant(
+      client,
+      roomId,
+      authorId,
+      "nickname",
+    );
+    await logic.participant.setParticipant(client, roomId, authorId, nickname, {
+      isDisconnected: false,
+    });
+    res.on("close", async () => {
       isRunning = false;
+      await logic.participant.setParticipant(
+        client,
+        roomId,
+        authorId,
+        nickname,
+        {
+          isDisconnected: true,
+        },
+      );
       const unsubscribe: (key: string) => Promise<string> = util
-        .promisify(client.unsubscribe)
-        .bind(client);
+        .promisify(pubsub.unsubscribe)
+        .bind(pubsub);
       await Promise.all(
         [chatKey, roomKey, participantIdKey].map((key) => unsubscribe(key)),
       );
+      res.end();
       await quit();
     });
-  } catch (error: unknown) {
+  } catch (error) {
     await quit();
-    if (error instanceof LogicError) {
-      context.status = error.code;
-      context.body = error.message;
-    } else if (error instanceof HttpError) {
-      if (error instanceof UnauthorizedError) {
-        context.set("WWW-Authenticate", error.realm);
-      }
-      context.status = error.code;
-      context.body = error.message;
-    } else if (error instanceof HttpResult) {
-      // eslint-disable-next-line no-console
-      console.error("You should not to throw HttpResult.");
-      context.status = 500;
-      context.body = "You should not to throw HttpResult.";
-    } else {
-      // unhandled error
-      // eslint-disable-next-line no-console
-      console.error(error);
-      context.status = 500;
-      context.body = String(error);
-    }
+    return next(error);
   }
 });
 
-koa.listen(5353);
+app.use((error: any, req: any, res: any, next: NextFunction) => {
+  if (res.headersSent) {
+    return next(error);
+  }
+  if (error instanceof LogicError) {
+    res.status(error.code).send(error.message);
+  } else if (error instanceof HttpError) {
+    if (error instanceof UnauthorizedError) {
+      res.set("WWW-Authenticate", error.realm);
+      res.status(error.code).end();
+    } else {
+      res.status(error.code).send(error.message);
+    }
+  } else if (error instanceof HttpResult) {
+    // eslint-disable-next-line no-console
+    console.error("You should not to throw HttpResult.");
+    res.status(error.code).send("You should not to throw HttpResult.");
+  } else {
+    // unhandled error
+    // eslint-disable-next-line no-console
+    console.error(error);
+    res.status(500).send(String(error));
+  }
+  res.status();
+});
+
+app.listen(5353, () => {
+  // eslint-disable-next-line no-console
+  console.log("app listen :5353");
+});

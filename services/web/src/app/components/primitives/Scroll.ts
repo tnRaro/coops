32a0e3e6cdffb2e63070cbe171bf0a240f7860ad
tnRaro <deokji53@gmail.com>
2021-06-09 11:25:30 +0900

    Add ScrollComponent

---
+++ services/web/src/app/components/primitives/Scroll.ts
@@ -0,0 +1,32 @@
+import { styled } from "../../stitches.config";
+
+import { Flex } from "./Flex";
+
+export const Scroll = styled(Flex, {
+  flex: "1 0 0",
+  $$scrollbarColor: "hsl(0deg 0% 0% / 20%)",
+  $$scrollbarThumb: "$colors$text33",
+  scrollbarWidth: "thin",
+  scrollbarColor: "$$scrollbarThumb $$scrollbarColor",
+  "&::-webkit-scrollbar": {
+    background: "$$scrollbarColor",
+    width: "8px",
+    borderRadius: "$full",
+  },
+  "&::-webkit-scrollbar-thumb": {
+    background: "$$scrollbarThumb",
+    borderRadius: "$full",
+  },
+  variants: {
+    x: {
+      true: {
+        overflowX: "auto",
+      },
+    },
+    y: {
+      true: {
+        overflowY: "auto",
+      },
+    },
+  },
+});

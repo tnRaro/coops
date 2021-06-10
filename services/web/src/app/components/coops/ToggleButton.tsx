10a3599b8364d3459bf3ba83e489c02fa8941f41
tnRaro <deokji53@gmail.com>
2021-06-10 22:54:12 +0900

    Add components

--- services/web/src/app/components/coops/ToggleButton.tsx
+++ services/web/src/app/components/coops/ToggleButton.tsx
@@ -1,6 +1,8 @@
 import { Toggle } from "@radix-ui/react-toggle";
-import React from "react";
+import { useAtomValue } from "jotai/utils";
+import React, { useState } from "react";
 
+import { isHostAtom, nicknameAtom } from "../../atoms";
 import { Button } from "../primitives/Button";
 
 interface ToggleButtonProps {
@@ -27,6 +29,57 @@ export const ToggleButton: React.VFC<ToggleButtonProps> = (props) => {
       }}
     >
       {props.pressed ? <props.On /> : <props.Off />}
+    </Button>
+  );
+};
+interface AudioControlButtonProps {
+  isLocalMuted?: boolean;
+  isSharingMuted?: boolean;
+  isServerMuted?: boolean;
+  isDisabled?: boolean;
+  onClick?: React.MouseEventHandler<HTMLButtonElement>;
+  MutedIcon: React.ElementType;
+  ServerMutedIcon: React.ElementType;
+  IdleStateIcon: React.ElementType;
+}
+export const AudioControlButton: React.VFC<AudioControlButtonProps> = (
+  props,
+) => {
+  const isHost = useAtomValue(isHostAtom);
+  const isDisabled = React.useMemo(() => {
+    if (props.isServerMuted && !isHost) return true;
+    return props.isDisabled ?? false;
+  }, [isHost, props.isDisabled, props.isServerMuted]);
+  const Icon = React.useMemo(() => {
+    if (props.isServerMuted) return props.ServerMutedIcon;
+    if (props.isLocalMuted) return props.MutedIcon;
+    if (props.isSharingMuted) return props.MutedIcon;
+    return props.IdleStateIcon;
+  }, [
+    props.IdleStateIcon,
+    props.MutedIcon,
+    props.ServerMutedIcon,
+    props.isLocalMuted,
+    props.isServerMuted,
+    props.isSharingMuted,
+  ]);
+  const color = React.useMemo(() => {
+    if (props.isServerMuted) return "var(--colors-dangerous66)";
+    if (props.isLocalMuted) return "var(--colors-dangerous66)";
+    if (props.isSharingMuted) return "var(--colors-text33)";
+    return undefined;
+  }, [props.isLocalMuted, props.isServerMuted, props.isSharingMuted]);
+  return (
+    <Button
+      disabled={isDisabled}
+      size="square8"
+      color="transparent"
+      style={{
+        color,
+      }}
+      onClick={props.onClick}
+    >
+      <Icon />
     </Button>
   );
 };

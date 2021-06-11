397bbad8420b156eaf510a190c81db9d92c93190
tnRaro <deokji53@gmail.com>
2021-06-12 06:26:21 +0900

    Remove unused import

--- services/logic/src/room/removeRoom.ts
+++ services/logic/src/room/removeRoom.ts
@@ -3,11 +3,9 @@ import { RedisClient } from "redis";
 
 import { clearParticipants } from "../participant/clearParticipants";
 
-import { createRoom } from "./createRoom";
-
 export const removeRoom = async (client: RedisClient, roomId: string) => {
-  await clearParticipants(client, roomId);
   await redis.chat.CURD.removeAllChats(client, roomId);
   await redis.room.CRUD.removeRoom(client, roomId);
   await redis.room.stream.removeRoom(client, roomId);
+  await clearParticipants(client, roomId);
 };
